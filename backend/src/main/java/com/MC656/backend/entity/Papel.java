17f3dff9e361905eb5df71af642f9b975e2edde4
Thiago Gradvohl de Oliveira <t281378@dac.unicamp.br>
2026-09-19 19:41:18 -0300

    feat(backend): cria entidade Participacao com fk para Usuario e Contexto

---
+++ backend/src/main/java/com/MC656/backend/entity/Papel.java
@@ -0,0 +1,12 @@
+package com.MC656.backend.entity;
+
+/*Esse eh o papel que po usuario exerce em determinado contexto (tipo eleitor, sindico, etc) */
+public enum Papel {
+    MEMBRO,
+    INTEGRANTE_CHAPA,
+    ADMINISTRADOR,
+    CONDOMINO,
+    SINDICO,
+    PARTICIPANTE_ASSEMBLEIA,
+    PROPONENTE
+}
