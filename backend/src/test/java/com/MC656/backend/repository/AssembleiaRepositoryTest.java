483a4abdb0af2a62cc2eeb7dc0ae4bcd1392c54e
Ana Luiza <a242389@dac.unicamp.br>
2026-09-21 15:24:54 -0300

    feat: testes automaticos relacionados ao centro academico

--- backend/src/test/java/com/MC656/backend/repository/AssembleiaRepositoryTest.java
+++ backend/src/test/java/com/MC656/backend/repository/AssembleiaRepositoryTest.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.MC656.backend.entity.*;
 
 @SpringBootTest 
